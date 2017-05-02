63c4b0ce66ca98f3486dfd373adcca01bdb69b30
Martin Kasmetski <spuder@kasmetski.com>
2017-05-02 16:10:59 +0300

    cosmetic fixes

--- common.go
+++ common.go
@@ -9,34 +9,41 @@ import (
 	"gobot.io/x/gobot/drivers/gpio"
 )
 
-//Ping the IP from Linux shell
-func Ping(ip string) bool {
-	out, _ := exec.Command("ping", ip, "-c 3", "-i 3", "-w 10").Output()
-	if strings.Contains(string(out), "Destination Host Unreachable") {
-		log.Println("HOST NOW FOUND - ", ip)
-		return false
-	}
-	log.Println("Host ", ip, " is online")
-	return true
-}
-
-//Restarter function logic
-func Restarter(rig Rig) {
-	log.Println("Restarting ", rig.name)
-	rig.pin.Off()
-	time.Sleep(5 * time.Second)
-	rig.pin.On()
-	time.Sleep(3 * time.Second)
-	rig.pin.Off()
-	time.Sleep(1 * time.Second)
-	rig.pin.On()
-	log.Println(rig.name, "restarted")
-}
-
 //Rig structure
 type Rig struct {
 	name string
 	pin  *gpio.RelayDriver
 	ip   string
 	info string
+}
+
+//Ping the IP from Linux shell
+func Ping(ip string) bool {
+	out, _ := exec.Command("ping", ip, "-c 3", "-i 3", "-w 10").Output()
+	if strings.Contains(string(out), "Destination Host Unreachable") {
+		return false
+	}
+	return true
+}
+
+//ForceShutDown machine
+func ForceShutDown(r Rig) {
+	r.pin.Off()
+	time.Sleep(5 * time.Second)
+	r.pin.On()
+}
+
+//TurnOn machine
+func TurnOn(r Rig) {
+	r.pin.Off()
+	time.Sleep(1 * time.Second)
+	r.pin.On()
+}
+
+//Restarter function logic
+func Restarter(r Rig) {
+	log.Println("### Restarting ", r.name)
+	ForceShutDown(r)
+	time.Sleep(3 * time.Second)
+	TurnOn(r)
 }
--- main.go
+++ main.go
@@ -11,24 +11,32 @@ import (
 
 func main() {
 	r := raspi.NewAdaptor()
-	//store the info in miningRigs map | using empty ip for testing purpose
+
+	//store the info in miningRigs map
 	//ToDo: config file
 	miningRigs := make(map[string]Rig)
-	miningRigs["rig1"] = Rig{"machine 1", gpio.NewRelayDriver(r, "38"), "192.168.0.111", "6 x RX480"}
-	miningRigs["rig2"] = Rig{"machine 2", gpio.NewRelayDriver(r, "40"), "192.168.0.111", "6 x RX470"}
+
+	miningRigs["rig1"] = Rig{"machine 1", gpio.NewRelayDriver(r, "38"), "192.168.0.100", "R9 290's"}
+	miningRigs["rig2"] = Rig{"machine 2", gpio.NewRelayDriver(r, "40"), "192.168.0.111", "RX480's"}
 
 	work := func() {
-		log.Println("Starting timer")
+		log.Println("# Starting timer")
+
 		//Check the machines every 10 minutes
 		gobot.Every(10*time.Minute, func() {
-			log.Println("Checking machines: ")
+			log.Println("# Checking machines: ")
+
 			for key, value := range miningRigs {
-				log.Println("Ping miner: ", key, " ip: ", value.ip)
+				log.Println("# Ping miner: ", key, " ip: ", value.ip)
 				if !Ping(value.ip) {
+					log.Println("### HOST NOT FOUND - ", value.name)
 					Restarter(miningRigs[key])
+				} else {
+					log.Println("# HOST IS ONLINE - ", value.name)
 				}
 			}
-
+			log.Println("# Checking machines DONE")
+			log.Println("# Starting timer")
 		})
 	}
 	robot := gobot.NewRobot("RPiMinerHardReset",
